f86a97b68bcb289a7d3512773634825ae35fe7d0
Bajron Ismailaj <caiupi@gmail.com>
2023-02-08 21:35:14 +0100

    section 7

--- source/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ source/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+console.log(document.querySelector('.message').textContent);
+
+console.log(document.querySelector('h1').textContent);
+//console.log(document.querySelector('body').textContent);
+//console.log(document.querySelector(''));
+
+document.querySelector('.message').textContent = 'Correct Number';
+document.querySelector('.number').textContent = '30';
+document.querySelector('.score').textContent = '40';
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
