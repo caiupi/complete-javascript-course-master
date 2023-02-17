028cfeab32eba6893eb22f27b7eaf17369978fb5
Bajron Ismailaj <caiupi@gmail.com>
2023-02-17 22:29:04 +0100

    83

--- source/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ source/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+console.log(score0El.textContent);
+console.log(score1El);
+console.log(diceEl);
+diceEl.classList.add('hidden');
