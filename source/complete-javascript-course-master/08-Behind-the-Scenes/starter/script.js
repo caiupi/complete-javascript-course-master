f40b445cb5eee96e2e8ca8b012c736616164138b
Bajron Ismailaj <caiupi@gmail.com>
2023-02-25 23:18:46 +0100

    13

--- source/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ source/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,24 @@
-'use strict';
+//'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  function printAge() {
+    const outpunt = `You are ${age} burn here ${birthYear}`;
+    console.log(outpunt);
+
+    if (1981 <= birthYear <= 1996) {
+      var str = ` You are a milleniar ${firstName}`;
+      console.log(str);
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(add(1, 2));
+    console.log(str);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Jonas';
+console.log(calcAge(1991));
