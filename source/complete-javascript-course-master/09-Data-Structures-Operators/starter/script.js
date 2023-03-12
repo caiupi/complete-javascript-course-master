fc66d666a6c66d15deee0e4a56f38c47f1b57178
Bajron Ismailaj <caiupi@gmail.com>
2023-03-12 18:17:00 +0100

    12

--- source/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ source/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,74 @@ const restaurant = {
       close: 24,
     },
   },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery: function ({ starterIndex = 1, mainIndex = 2, time, adress }) {
+    console.log(starterIndex);
+
+    console.log(this.categories[1]);
+    console.log(`order receved ${this.mainMenu[mainIndex]}  w`);
+  },
 };
+
+restaurant.orderDelivery({
+  time: '22:30',
+  adress: 'Via del sole 21',
+  mainIndex: 1,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  time: '22:30',
+  adress: 'Via del sole 21',
+});
+console.log('---------');
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName);
+console.log(hours);
+console.log(tags);
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+//console.log(fri);
+console.log(o, c);
+
+/*
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+const [first, , second] = restaurant.categories;
+console.log(first, second);
+let [main, secondary] = restaurant.categories;
+[main, secondary] = [secondary, main];
+console.log(secondary, main);
+console.log(restaurant.order(2, 0));
+const [starter, demain] = restaurant.order(2, 0);
+console.log(starter, demain);
+const nested = [2, 4, [5, 6]];
+//const [i, , j] = nested;
+//console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
+*/
